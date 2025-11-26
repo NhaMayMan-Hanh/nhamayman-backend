f486c4240cb4679153caa8aa49c7d18d588f84c2
Nkduy23 <nguyenkhanhduy23803@gmail.com>
2025-11-26 23:34:32 +0700

    Update auth.controller.ts

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -38,7 +38,6 @@ export const loginController = async (req: Request, res: Response) => {
       secure: true, // ✅ Hoặc process.env.NODE_ENV === "production"
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 ngày
       path: "/", // ✅ Đảm bảo cookie available cho toàn bộ domain
-      domain: "nhamayman-frontend.vercel.app"
     });
     res.json({
       success: true,
