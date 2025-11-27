515b8149aea1b1a6c5e64e88899116609794c317
Nkduy23 <nguyenkhanhduy23803@gmail.com>
2025-11-27 13:47:16 +0700

    test

--- src/server.ts
+++ src/server.ts
@@ -24,6 +24,8 @@ app.use(
   cors({
     origin: process.env.CLIENT_URL,
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
   })
 );
 
